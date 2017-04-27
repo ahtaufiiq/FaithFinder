8fbfb830e081f383243b463111ccebdc9e9cfe9a
ataufiq123 <ataufiq665@gmail.com>
2017-04-28 03:15:30 +0700

    asd

---
+++ app/src/main/java/com/example/taufik/masjid/MasjidDetectorApplication.java
@@ -0,0 +1,21 @@
+package com.example.taufik.masjid;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+public class MasjidDetectorApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    //for multidex
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        MultiDex.install(this);
+    }
+}
+
